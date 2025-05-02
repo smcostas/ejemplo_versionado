dcbca54921bd0e54502d2aa1c161459d1cb09a92
SantiCostas <scostas@imbiv.unc.edu.ar>
2025-05-02 13:26:53 -0300

    al archivo anterior le agregue codigo simulando datos y grafico

---
+++ ver1.py
@@ -0,0 +1,21 @@
+### quiero simular datos de una funcion log normal y hacer un histograma
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from random import random, seed
+import math
+import scipy.stats as stats
+### ejecuta lo que pedi
+# seed(1)
+# np.random.seed(1) # para reproducibilidad
+
+mu = 0.5
+sigma = 0.5
+n = 10000
+x = np.random.lognormal(mu, sigma, n)
+plt.figsize(10, 6)
+sns.histplot(x, bins=30, kde=True, color='blue', alpha=0.5)
+plt.title('Histograma de datos log-normales')
+plt.show()
+
